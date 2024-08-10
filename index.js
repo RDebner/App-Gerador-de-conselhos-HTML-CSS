8f23dba6046b17ca06956c274b4e35a17b0fabff
Rafael Debner <rafaeldebner08@gmail.com>
2024-08-10 02:45:32 -0300

    Adicionando JavaScript

---
+++ index.js
@@ -0,0 +1,34 @@
+const textoConselho = document.getElementById('text') 
+const idConselho = document.getElementById('idConselho')
+const botao = document.getElementById('button')
+
+async function gerarConselho () {
+    const url = "https://api.adviceslip.com/advice"
+    const conselho = await fetch(url)
+    return await conselho.json()
+}
+
+async function mensagemConselho () {
+    const conselho = await gerarConselho()
+    const mensagem = conselho.slip.advice
+    return mensagem
+}
+
+async function idDoConselho () {
+    const conselho = await gerarConselho()
+    const id = conselho.slip.id
+    return id
+}
+
+
+botao.addEventListener('click' , async function exibirConselho () {
+    idConselho.innerText = await idDoConselho()
+    textoConselho.innerText = await mensagemConselho()
+} )
+
+
+
+
+
+
+
